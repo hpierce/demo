4538f82b751ddab4376fe437930fca5d7598b82d
Hugh Pierce <hpierce3213@gmail.com>
2019-03-27 13:50:27 -0400

    rewrites

--- display.php
+++ display.php
@@ -41,8 +41,9 @@
 	echo "<html>
 <head>
 <title>Account $number</title>
-</head>
-<body bgcolor=ffffff><font face=arial,helvetica><br><br>
+</head>\n";
+
+echo "<body bgcolor=ffffff><font face=arial,helvetica><br><br>
 <h1 align=center>Account: $number<br>
 Service Address: $sa<br>
 Name on Invoice: $noi<br>
@@ -52,7 +53,7 @@ Name on Invoice: $noi<br>
 #	Display
 #
 #---------------------------------------------------------------------------
-	$query = "select * from main where account_number = '$number' order by filename";
+	$query = "select * from main where account_number = '$number' order by bill_date";
 	$result = mysqli_query($db, $query);
 	$num_results = mysqli_num_rows($result);
 #---------------------------------------------------------------------------
@@ -61,51 +62,51 @@ Name on Invoice: $noi<br>
 #
 #---------------------------------------------------------------------------
 	echo "<center>
-<p><table border=1 cellspacing=0 cellpadding=4 width=80%>
+<p><table border=1 cellspacing=0 cellpadding=4>
+<tr><td align=center>A</td><td align=center>B</td>
+<td align=center>C</td><td align=center>D</td>
+<td align=center>E</td><td align=center>F</td>
+<td align=center>G</td> <td align=center>H</td><td align=center>I</td><td align=center>J</td></tr>
 <tr><td bgcolor=cccccc align=left>Bill Date</td>
-<td bgcolor=cccccc align=left>Supplier 1 Name</td><td bgcolor=cccccc align=left>Supplier 1 Amount</td><td bgcolor=cccccc align=left>Supplier2 Name</td><td bgcolor=cccccc align=left>Supplier 2 Amount</td>
-<td bgcolor=cccccc align=left>New Charges</td><td bgcolor=cccccc align=left>Balance Forward</td><td bgcolor=cccccc align=left>Total Credit Amount</td><td bgcolor=cccccc align=left>Total Amount Due</td>
-<td bgcolor=cccccc align=left>Meter Start</td><td bgcolor=cccccc align=left>Meter End</td><td bgcolor=cccccc align=left>Next Meter Read</td>
-<td bgcolor=cccccc align=left>Number of Days</td></font></tr>\n";
+<td bgcolor=cccccc align=left>Supplier 1 Amount</td>
+<td bgcolor=cccccc align=left>Supplier 2 Amount</td>
+<td bgcolor=cccccc align=left>New Charges</td>
+<td bgcolor=cccccc align=left>Balance Forward</td>
+<td bgcolor=cccccc align=left>Sum of B-E</td>
+<td bgcolor=cccccc align=left>Total Credit Amount</td>
+<td bgcolor=cccccc align=left>Total Amount Due</td>
+<td bgcolor=cccccc align=left>Sum of G-H</td>
+<td bgcolor=cccccc align=left>Subtract F-I</td>
+</tr>\n";
 #---------------------------------------------------------------------------
 #
 #	Data
 #
 #---------------------------------------------------------------------------
-
 	for ($i=0; $i <$num_results; $i++)
 	{
 		$row = mysqli_fetch_array($result);
-		echo '<tr><td><a href=MD/';
-		echo stripslashes($row['filename']);
-		echo '>';
-		echo stripslashes($row['bill_date']);
-		echo '</a><br></td><td>';
-		echo stripslashes($row['supplier1_name']);
-		echo '<br></td><td>';
-		echo stripslashes($row['supplier1_amount']);
-		echo '<br></td><td>';
-		echo stripslashes($row['supplier2_name']);
-		echo '<br></td><td>';
-		echo stripslashes($row['supplier2_amount']);
-		echo '<br></td><td>';
-		echo stripslashes($row['new_charges']);
-		echo '<br></td><td>';
-		echo stripslashes($row['balance_forward']);
-		echo '<br></td><td>';
-		echo stripslashes($row['total_credit_amount']);
-		echo '<br></td><td>';
-		echo stripslashes($row['total_amount_due']);
-		echo '<br></td><td>';
-		echo stripslashes($row['meter_start']);
-		echo '<br></td><td>';
-		echo stripslashes($row['meter_end']);
-		echo '<br></td><td>';
-		echo stripslashes($row['next_meter_read']);
-		echo '<br></td><td>';
-		echo stripslashes($row['number_of_days']);
-		echo "<br></td></tr>\n";
+		$fn = stripslashes($row['filename']);
+		$bd = stripslashes($row['bill_date']);
+		$sup1 = stripslashes($row['supplier1_amount']);
+		$sup2 = stripslashes($row['supplier2_amount']);
+		$new = stripslashes($row['new_charges']);
+		$bf = stripslashes($row['balance_forward']);
+		$billa = $sup1 + $sup2 + $new + $bf;
+
+		$tca = stripslashes($row['total_credit_amount']);
+		$tad = stripslashes($row['total_amount_due']);
+		$billb = $tca + $tad;
+
+		$good = abs(round($billa - $billb,2));
+
+		echo "<tr><td><a href=files/$fn>$bd</a></td><td>$sup1</td><td>$sup2</td><td>$new</td><td>$bf</td><td>$billa</td><td>$tca</td><td>$tad</td><td>$billb</td><td>$good</td></tr>";
 	}
-	echo "</table></center>\n";
-	echo "<br><br></body></html>\n";
+	echo "</table>
+<br><table border=1 cellspacing=0 cellpadding=4>
+<tr><td><img src=images/accounting.jpg></td>
+<td><img src=images/cost.jpg></td>
+<td><img src=images/comparison.jpg></td></tr>
+</table>
+<br><br></body></html>\n";
 ?>
--- index.php
+++ index.php
@@ -34,57 +34,54 @@
 <body bgcolor=ffffff><font face=arial,helvetica><h1 align=center>Pepco Database</h1><center>
 <br>
 <p><table border=1 cellspacing=0 cellpadding=4 width=80%>
-<tr><font size=+1><td bgcolor=cccccc align=left>Name on Invoice</td><td bgcolor=cccccc align=left>Service Address</td><td bgcolor=cccccc align=left>Account Number</td>
-<td bgcolor=cccccc align=left>Supplier 1 Name</td><td bgcolor=cccccc align=left>Supplier 1 Amount</td><td bgcolor=cccccc align=left>Supplier2 Name</td><td bgcolor=cccccc align=left>Supplier 2 Amount</td>
-<td bgcolor=cccccc align=left>New Charges</td><td bgcolor=cccccc align=left>Balance Forward</td><td bgcolor=cccccc align=left>Total Credit Amount</td><td bgcolor=cccccc align=left>Total Amount Due</td>
-<td bgcolor=cccccc align=left>Bill Date</td><td bgcolor=cccccc align=left>Meter Start</td><td bgcolor=cccccc align=left>Meter End</td><td bgcolor=cccccc align=left>Next Meter Read</td>
-<td bgcolor=cccccc align=left>Number of Days</td></font></tr>\n";
+<tr>
+<font size=+1>
+<td bgcolor=cccccc align=left>Name on Invoice</td>
+<td bgcolor=cccccc align=left>Service Address</td>
+<td bgcolor=cccccc align=left>Account Number</td>
+<td bgcolor=cccccc align=left>Supplier 1 Name</td>
+<td bgcolor=cccccc align=left>Supplier 1 Amount</td>
+<td bgcolor=cccccc align=left>Supplier2 Name</td>
+<td bgcolor=cccccc align=left>Supplier 2 Amount</td>
+<td bgcolor=cccccc align=left>New Charges</td>
+<td bgcolor=cccccc align=left>Balance Forward</td>
+<td bgcolor=cccccc align=left>Total Credit Amount</td>
+<td bgcolor=cccccc align=left>Total Amount Due</td>
+<td bgcolor=cccccc align=left>Bill Date</td>
+<td bgcolor=cccccc align=left>Meter Start</td>
+<td bgcolor=cccccc align=left>Meter End</td>
+<td bgcolor=cccccc align=left>Next Meter Read</td>
+<td bgcolor=cccccc align=left>Number of Days</td>
+</font>
+</tr>\n";
 
-	$query = "select * from main order by filename";
+	$query = "select * from main order by account_number,bill_date";
 	$result = mysqli_query($db, $query);
 	$num_results = mysqli_num_rows($result);
 
 	for ($i=0; $i <$num_results; $i++)
 	{
 		$row = mysqli_fetch_array($result);
-		echo '<tr><td><a href=MD/';
-		echo stripslashes($row['filename']);
-		echo '>';
-		echo stripslashes($row['name_on_invoice']);
-		echo '</a><br></td><td>';
-		echo stripslashes($row['service_address']);
-		echo '<br></td><td><a href=display.php?number=';
-		echo stripslashes($row['account_number']);
-		echo '>';
-		echo stripslashes($row['account_number']);
-		echo '</a><br></td><td>';
-		echo stripslashes($row['supplier1_name']);
-		echo '<br></td><td>';
-		echo stripslashes($row['supplier1_amount']);
-		echo '<br></td><td>';
-		echo stripslashes($row['supplier2_name']);
-		echo '<br></td><td>';
-		echo stripslashes($row['supplier2_amount']);
-		echo '<br></td><td>';
-		echo stripslashes($row['new_charges']);
-		echo '<br></td><td>';
-		echo stripslashes($row['balance_forward']);
-		echo '<br></td><td>';
-		echo stripslashes($row['total_credit_amount']);
-		echo '<br></td><td>';
-		echo stripslashes($row['total_amount_due']);
-		echo '<br></td><td>';
-		echo stripslashes($row['bill_date']);
-		echo '<br></td><td>';
-		echo stripslashes($row['meter_start']);
-		echo '<br></td><td>';
-		echo stripslashes($row['meter_end']);
-		echo '<br></td><td>';
-		echo stripslashes($row['next_meter_read']);
-		echo '<br></td><td>';
-		echo stripslashes($row['number_of_days']);
-		echo "<br></td></tr>\n";
+		$fn = stripslashes($row['filename']);
+		$noi = stripslashes($row['name_on_invoice']);
+		$sa = stripslashes($row['service_address']);
+		$an = stripslashes($row['account_number']);
+		$sup1n = stripslashes($row['supplier1_name']);
+		$sup1a = stripslashes($row['supplier1_amount']);
+		$sup2n = stripslashes($row['supplier2_name']);
+		$sup2a = stripslashes($row['supplier2_amount']);
+		$new = stripslashes($row['new_charges']);
+		$bf = stripslashes($row['balance_forward']);
+		$tca =  stripslashes($row['total_credit_amount']);
+		$tad = stripslashes($row['total_amount_due']);
+		$bd = stripslashes($row['bill_date']);
+		$mets = stripslashes($row['meter_start']);
+		$mete = stripslashes($row['meter_end']);
+		$metn = stripslashes($row['next_meter_read']);
+		$nod = stripslashes($row['number_of_days']);
+
+		echo "<tr><td><a href=files/$fn>$noi</a><br></td><td>$sa<br></td><td><a href=display.php?number=$an>$an</a><br></td><td>$sup1n<br></td><td>$sup1a<br></td><td>$sup2n<br></td><td>$sup2a<br></td><td>$new<br></td><td>$bf<br></td><td>$tca<br></td><td>$tad<br></td><td>$bd<br></td><td>$mets<br></td><td>$mete<br></td><td>$metn<br></td><td>$nod<br></td></tr>\n";
 	}
-	echo "</table></center>\n";
-	echo "<br><br></body></html>\n";
+	echo "</table></center>
+<br><br></body></html>\n";
 ?>
